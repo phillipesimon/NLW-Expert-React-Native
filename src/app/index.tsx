6c5f776971d5897941662135ec418a596e9ae179
Simon <phillipe.simon@gmail.com>
2024-02-13 20:39:15 -0300

    Aula 01

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,34 @@
-import { Text } from "react-native";
+import { CategoryButton } from "@/components/category-button";
+import { Header } from "@/components/header";
+import { FlatList, View } from "react-native";
+import { CATEGORIES } from "@/utils/data/products";
+import { useState } from "react";
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+  }
+
   return (
-    <>
-      <Text>Home NLW Expert</Text>
-    </>
+    <View className=" flex-1 pt-8 ">
+      <Header title="Faça seu pedido" cartQuantityItems={5} />
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+    </View>
   );
 }
